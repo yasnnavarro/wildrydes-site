c2c91735ff4c017b3b5f0a35a7fbeaac1db58c61
yasnnavarro <yasnnavarro@gmail.com>
2025-11-19 16:45:26 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: '', // e.g. us-east-1_cxuEfsMxm
+        userPoolClientId: '', // e.g. pfnoosuvv4m3o1d3v5olabui2
+        region: '' // e.g. us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
