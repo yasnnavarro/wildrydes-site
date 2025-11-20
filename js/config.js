13e99b34b733f86fd13944804f4f0878acddaa90
yasnnavarro <yasnnavarro@gmail.com>
2025-11-20 12:15:35 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_cxuEfsMxm', // e.g. us-east-1_cxuEfsMxm
-        userPoolClientId: '24hdnpsachlif97uptfn6rouod', // e.g. pfnoosuvv4m3o1d3v5olabui2
+        userPoolId: 'us-east-1_97haxqCIj', // e.g. us-east-1_cxuEfsMxm
+        userPoolClientId: '5cs3p5khbr7dvat02ht3g5d049', // e.g. pfnoosuvv4m3o1d3v5olabui2
         region: 'us-east-1' // e.g. us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
